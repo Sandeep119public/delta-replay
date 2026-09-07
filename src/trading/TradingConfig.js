ed65b59da076b17eebff70bebd218cb525d4411c
Sandeep119public <Sandeepchanda119public@gmail.com>
2026-09-07 19:08:16 +0530

    refactor(v2): remove superseded legacy JavaScript trading engine

--- src/trading/TradingConfig.js
+++
@@ -1,9 +0,0 @@
-import { FeeCalculator } from './FeeCalculator.js';
-
-export const TRADING_CONFIG = {
-  TAKER_FEE_RATE: 0.0005, // 0.05%
-};
-
-export function calcFee(notional, rate = TRADING_CONFIG.TAKER_FEE_RATE) {
-  return FeeCalculator.calculate(notional, rate);
-}
