519836834441b6669438552dd0dd17d0c546b2d1
Sandeep119public <Sandeepchanda119public@gmail.com>
2026-09-16 14:44:23 +0530

    fix: return application page host from layout renderer

--- src/ui/paper/PaperLayout.js
+++ src/ui/paper/PaperLayout.js
@@ -2,7 +2,7 @@ import { paperMarkup } from '../paperMarkup.js';
 
 export function renderPaperLayout(mount) {
   if (!mount) throw new Error('Paper UI mount is required');
-  if (mount.querySelector?.('#page-replay')) return mount.firstElementChild;
+  if (mount.querySelector?.('#page-replay')) return mount.querySelector('#page-host') || mount.firstElementChild;
   mount.innerHTML = paperMarkup();
-  return mount.firstElementChild;
+  return mount.querySelector('#page-host') || mount.firstElementChild;
 }
