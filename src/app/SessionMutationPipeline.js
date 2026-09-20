096896019465447a8f6fa3e5a47be360ca2500c7
Sandeep119public <Sandeepchanda119public@gmail.com>
2026-09-20 19:50:00 +0530

    refactor: simplify session mutation pipeline

--- src/app/SessionMutationPipeline.js
+++ src/app/SessionMutationPipeline.js
@@ -3,8 +3,6 @@ export class SessionMutationPipeline {
     this._tail = null;
     this._generation = 0;
     this._destroyed = false;
-    this._nextSequence = 0;
-    this._latestSequence = new Map();
   }
 
   generation() {
@@ -19,40 +17,25 @@ export class SessionMutationPipeline {
   run(operation, {
     generation = this._generation,
     apply = null,
-    scope = 'default',
-    serialize = true,
     canExecute = null,
   } = {}) {
-    if (this._destroyed) return Promise.resolve({ applied: false, stale: true, response: null });
+    if (this._destroyed) {
+      return Promise.resolve({ applied: false, stale: true, response: null });
+    }
 
-    const sequence = ++this._nextSequence;
     const execute = async () => {
       if (this._destroyed || (typeof canExecute === 'function' && !canExecute())) {
         return { applied: false, stale: true, response: null };
       }
 
       const response = await operation();
-      const latestSequence = this._latestSequence.get(scope) || 0;
-      const stale = !serialize && sequence < latestSequence;
-      const applied = !this._destroyed && !stale && generation === this._generation;
-
-      if (!stale && !this._destroyed) this._latestSequence.set(scope, sequence);
+      const applied = !this._destroyed && generation === this._generation;
       if (applied && typeof apply === 'function') apply(response);
-      return { applied, stale, response };
+      return { applied, stale: !applied, response };
     };
 
-    if (!serialize) return execute();
-
-    if (!this._tail) {
-      const next = execute();
-      const tracked = next.catch(() => undefined);
-      this._tail = tracked;
-      return next;
-    }
-
-    const next = this._tail.then(execute, execute);
-    const tracked = next.catch(() => undefined);
-    this._tail = tracked;
+    const next = this._tail ? this._tail.then(execute, execute) : execute();
+    this._tail = next.catch(() => undefined);
     return next;
   }
 
@@ -60,6 +43,5 @@ export class SessionMutationPipeline {
     this._destroyed = true;
     this._generation += 1;
     this._tail = null;
-    this._latestSequence.clear();
   }
 }
